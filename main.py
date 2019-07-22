e9abfc61e20c356e9b125fc9c9663e411aee3df1
yavuzdrmzksr <39765615+yavuzdrmzksr@users.noreply.github.com>
2019-07-22 11:03:09 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from mail_sender import send_mail
 
 def main():
-    send_mail("../churn-ratio-(saas).csv")
+    send_mail("../p30-(paid-users-who-upgraded-in-the-first-30-days)-(saas).csv","$")
 
 if __name__ == "__main__":
     main()
